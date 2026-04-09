f002f91ccb8487a312eaa0ac4d2cc0d19d51cd5c
Fawwad Aslam <fawwadaslam@hotmail.com>
2026-04-09 17:59:43 +0500

    Add files via upload
    I made Maths calculator using slightly different code

---
+++ Maths Calculator B.py
@@ -0,0 +1,49 @@
+def add(n1, n2):
+    return n1 + n2
+def subtract(n1, n2):
+    return n1 - n2
+def divide(n1, n2):
+    return n1 / n2
+def multiply(n1, n2):
+    return n1 * n2
+
+operators = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+    }
+loop = True
+while loop:
+    print(r''' _____________________
+    |  _________________  |
+    | | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+    | |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+    |  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+    | | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+    | |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+    | | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+    | |___|___|___| |___| | | |  \ '.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ '.___.'\  | |
+    | | 1 | 2 | 3 | | x | | | |   '._____.'  | || ||____|  |____|| || |  |________|  | || |   '._____.'  | |
+    | |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+    | | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+    | |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+    |_____________________|''')
+
+
+    first_number = float(input("Choose first number: "))
+    running = True
+    while running:
+        for symbols in operators:
+            print(symbols)
+        functions = input("Choose the function: ")
+        second_number = float(input("Choose second number: "))
+        calculation = operators[functions](first_number, second_number)
+        print(f"{first_number} {functions} {second_number} = {calculation}")
+        option = input(f"Type 'y' to continue calculating with {calculation}, or type 'n' to start a new calculation: ")
+
+        if option == "y":
+            first_number = calculation
+        else:
+            running = False
+            print("\n"*30)
